425ddfdb80be197f2606c5287d16a81826b56a65
MOSTAFA6499 <mostafazarei9292@gmail.com>
2026-06-05 03:31:32 +0330

    Update appy.py

--- appy.py
+++ appy.py
@@ -1,1 +1,5 @@
-from titan_core.wsgi import application
+import os
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'titan_core.settings')
+application = get_wsgi_application()
